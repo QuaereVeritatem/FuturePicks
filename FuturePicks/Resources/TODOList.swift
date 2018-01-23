3ffb79d82c001e605b2f8b626c22afbd2bf35c9d
Robert <robert.lee.martin@gmail.com>
2018-01-23 17:40:22 -0600

    Created Todo list
    Created Todo list, a one stop location for what features I want to do in the future and to bugs/housekeeping tasks that still need to be fixed/done

---
+++ FuturePicks/Resources/TODOList.swift
@@ -0,0 +1,22 @@
+//
+//  TODOList.swift
+//  FuturePicks
+//
+//  Created by Robert Martin on 1/15/18.
+//  Copyright © 2018 Robert Martin. All rights reserved.
+//
+
+import Foundation
+
+/*
+ This is the TODO list of items that need to be done:
+ 1) Put past picks for MegaMillions into database
+ 2) Put past picks for Lotto Texas into database
+ 3) Put past picks in permanent/persistent memory(possibly CoreData)
+ 4) *** Have random pick results put in ascending order
+ 5) Future feature: ability to search through past random number picks?
+ 6) start using "// MARK: - Navigation" (or other thinsg besides navigation) to find things easier
+ 7) change storyboard to reflect changes in the sketch regarding distances between banners
+ 
+ 
+ */
